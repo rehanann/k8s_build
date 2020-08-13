83d47fbed58dd059f5aa04b4f4edf06b4a452cc3
Rehan Anwar <rehanann@gmail.com>
2020-08-13 09:37:22 +0100

    change hostnames

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "master" {
-  name = "master-1"
+resource "google_compute_instance" "controller" {
+  name = "controller-1"
   machine_type = "${var.machine_type}"
   zone = "${"${var.region}"}-a"
 
@@ -40,20 +40,20 @@ resource "google_compute_instance" "master" {
 
 }
 
-resource "google_compute_disk" "master" {
-    name = "mdocker"
+resource "google_compute_disk" "controller-1" {
+    name = "cdocker"
     type = "pd-ssd"
     zone = "${"${var.region}"}-a"
     size = "10"
 }
 
-resource "google_compute_attached_disk" "master" {
-    disk = "${google_compute_disk.master.self_link}"
-    instance = "${google_compute_instance.master.self_link}"
+resource "google_compute_attached_disk" "controller-1" {
+    disk = "${google_compute_disk.controller-1.self_link}"
+    instance = "${google_compute_instance.controller-1.self_link}"
 }
 
-resource "google_compute_instance" "infra" {
-  name = "infra-1"
+resource "google_compute_instance" "minion" {
+  name = "minion-1"
   machine_type = "${var.machine_type}"
   zone = "${"${var.region}"}-a"
 
@@ -94,68 +94,68 @@ resource "google_compute_instance" "infra" {
 
 }
 
-resource "google_compute_disk" "infra" {
-    name = "idocker"
+resource "google_compute_disk" "minion" {
+    name = "mindocker"
     type = "pd-ssd"
     zone = "${"${var.region}"}-a"
     size = "10"
 }
 
-resource "google_compute_attached_disk" "infra" {
-    disk = "${google_compute_disk.infra.self_link}"
-    instance = "${google_compute_instance.infra.self_link}"
+resource "google_compute_attached_disk" "minion" {
+    disk = "${google_compute_disk.minion.self_link}"
+    instance = "${google_compute_instance.minion.self_link}"
 }
 
-resource "google_compute_instance" "worker" {
-  name = "worker-1"
-  machine_type = "${var.machine_type}"
-  zone = "${"${var.region}"}-a"
+# resource "google_compute_instance" "worker" {
+#   name = "worker-1"
+#   machine_type = "${var.machine_type}"
+#   zone = "${"${var.region}"}-a"
 
 
-  boot_disk {
-      initialize_params {
-          image = "${var.image}"
-          size = "20"
-      }
-  }
+#   boot_disk {
+#       initialize_params {
+#           image = "${var.image}"
+#           size = "20"
+#       }
+#   }
 
-  network_interface {
-      network = "default"
+#   network_interface {
+#       network = "default"
 
-      access_config { 
-          // Ephemeral IP
-      }
-  }
+#       access_config { 
+#           // Ephemeral IP
+#       }
+#   }
 
-  service_account {
-      scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
+#   service_account {
+#       scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+#   }
 
-  metadata = {
-    ssh-keys = "${var.username}:${file("${var.path}/jenkins_key")}"
-  }
+#   metadata = {
+#     ssh-keys = "${var.username}:${file("${var.path}/jenkins_key")}"
+#   }
 
-  provisioner "file" {
-    source = "${file("${var.path}/sshd_config")}"
-    destination = "/etc/ssh/sshd_config"
-  }
+#   provisioner "file" {
+#     source = "${file("${var.path}/sshd_config")}"
+#     destination = "/etc/ssh/sshd_config"
+#   }
 
-  connection {
-    user = "root"
-    type = "ssh"
-    private_key = "${file("${var.path}/rsa_root_key")}"
-  }
+#   connection {
+#     user = "root"
+#     type = "ssh"
+#     private_key = "${file("${var.path}/rsa_root_key")}"
+#   }
   
-}
+# }
 
-resource "google_compute_disk" "worker" {
-    name = "wdocker"
-    type = "pd-ssd"
-    zone = "${"${var.region}"}-a"
-    size = "10"
-}
+# resource "google_compute_disk" "worker" {
+#     name = "wdocker"
+#     type = "pd-ssd"
+#     zone = "${"${var.region}"}-a"
+#     size = "10"
+# }
 
-resource "google_compute_attached_disk" "worker" {
-    disk = "${google_compute_disk.worker.self_link}"
-    instance = "${google_compute_instance.worker.self_link}"
-}
+# resource "google_compute_attached_disk" "worker" {
+#     disk = "${google_compute_disk.worker.self_link}"
+#     instance = "${google_compute_instance.worker.self_link}"
+# }
