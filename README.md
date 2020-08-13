39865b7344c8db5456ede58ae65003b28bf5718c
Rehan Anwar <rehanann@gmail.com>
2020-08-13 21:59:42 +0100

    include ansible scripts

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# terraform-ansible
+# Deme: Kubernetes 1.17 installation
+
+## Archicture
+* Master 1
+* worker 1
+
+## deployment method:
+* Use Google CLoud platform, and for Infra autoation Jenkins 2, Terraform 0.12.x, and application deployment with Ansible 2.9.
+* Put GCP credentials in Jenkins secrets, also for security reasons and donot expose GCP project id copy terraform provider.tf and variables.tf in jenkins secrets.
+*  Find nodes deployments in Terraform main.tf file and kubernetes installation ansible playbooks in the ansible directory.
+* additional steps: create overlay2 filesystem for docker storage.
+## steps for manual installation
+* git clone https://github.com/rehanann/k8s_build.git
+* terraform init
+* terraform plan -out myplan
+* terraform plan -out myplan
+* ansible-playbook -i host.ini ansible/config.yml
+* ansible-playbook -i host.ini ansible/docker-storage-setup-ofs.yml
+* ansible-playbook -i host.ini ansible/masters.yml
+* ansible-playbook -i host.ini ansible/worker.yml
+  
+
--- main.tf
+++ main.tf
@@ -27,16 +27,6 @@ resource "google_compute_instance" "controller" {
     ssh-keys = "${var.username}:${file("${var.path}/jenkins_key")}"
   }
 
-  # provisioner "file" {
-  #   source = "${file("${var.path}/sshd_config")}"
-  #   destination = "/etc/ssh/sshd_config"
-  # }
-
-  # connection {
-  #   user = "root"
-  #   type = "ssh"
-  #   private_key = "${file("${var.path}/rsa_root_key")}"
-  # }
 
 }
 
@@ -81,17 +71,6 @@ resource "google_compute_instance" "minion" {
     ssh-keys = "${var.username}:${file("${var.path}/jenkins_key")}"
   }
 
-  # provisioner "file" {
-  #   source = "${file("${var.path}/sshd_config")}"
-  #   destination = "/etc/ssh/sshd_config"
-  # }
-
-  # connection {
-  #   user = "root"
-  #   type = "ssh"
-  #   private_key = "${file("${var.path}/rsa_root_key")}"
-  # }
-
 }
 
 resource "google_compute_disk" "minion" {
@@ -105,57 +84,3 @@ resource "google_compute_attached_disk" "minion" {
     disk = "${google_compute_disk.minion.self_link}"
     instance = "${google_compute_instance.minion.self_link}"
 }
-
-# resource "google_compute_instance" "worker" {
-#   name = "worker-1"
-#   machine_type = "${var.machine_type}"
-#   zone = "${"${var.region}"}-a"
-
-
-#   boot_disk {
-#       initialize_params {
-#           image = "${var.image}"
-#           size = "20"
-#       }
-#   }
-
-#   network_interface {
-#       network = "default"
-
-#       access_config { 
-#           // Ephemeral IP
-#       }
-#   }
-
-#   service_account {
-#       scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-#   }
-
-#   metadata = {
-#     ssh-keys = "${var.username}:${file("${var.path}/jenkins_key")}"
-#   }
-
-#   provisioner "file" {
-#     source = "${file("${var.path}/sshd_config")}"
-#     destination = "/etc/ssh/sshd_config"
-#   }
-
-#   connection {
-#     user = "root"
-#     type = "ssh"
-#     private_key = "${file("${var.path}/rsa_root_key")}"
-#   }
-  
-# }
-
-# resource "google_compute_disk" "worker" {
-#     name = "wdocker"
-#     type = "pd-ssd"
-#     zone = "${"${var.region}"}-a"
-#     size = "10"
-# }
-
-# resource "google_compute_attached_disk" "worker" {
-#     disk = "${google_compute_disk.worker.self_link}"
-#     instance = "${google_compute_instance.worker.self_link}"
-# }
